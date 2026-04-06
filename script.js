7575970eb0e3f47a46a4a2e1dc387535b0c368b8
Ілля Кравцов <ilya.dmtr.kravtsov@gmail.com>
2026-04-06 20:05:31 +0300

    hw48 done

--- script.js
+++ script.js
@@ -34,3 +34,63 @@ console.log(set1)
 console.log(set1.size === 1);
 
 // false, бо Set порівнює не значення, а посилання, тому додає обидва масива
+
+
+// 1. Запитайте n
+// Створіть масив з n натуральних елементів від 1 до 10
+// reduce його. в результаті отримаємо 
+// {
+// odd: 2, 
+// even: 8
+// }
+
+
+const n = Number(prompt("Введіть кількість елементів"));
+
+const arr2 = Array.from({ length: n }, () => Math.floor(Math.random() * 10) + 1);
+
+console.log("Масив:", arr);
+
+const result1 = arr.reduce((acc, num) => {
+  if (num % 2 === 0) {
+    acc.even++;
+  } else {
+    acc.odd++;
+  }
+  return acc;
+}, { odd: 0, even: 0 });
+
+console.log(result1);
+
+// 2.
+// const users = [
+//   { login: "user_1", age: 12 },
+//   { login: "devMax", age: 19 },
+//   { login: "codeMaster", age: 17 },
+//   { login: "frontend_guy", age: 22 },
+//   { login: "backend_pro", age: 16 },
+//   { login: "jsNinja", age: 27 },
+//   { login: "reactFan", age: 14 },
+//   { login: "nodeHero", age: 31 },
+//   { login: "fullstack_dev", age: 18 },
+//   { login: "testerQA", age: 15 }
+// ];
+//       - додайте всім по 1 року (дослідити)
+//       - створіть масив reduce з логінів повнолітніх
+
+// const n = Number(prompt("Введіть кількість елементів"));
+
+// const arr = Array.from({ length: n }, () => Math.floor(Math.random() * 10) + 1);
+
+// console.log("Масив:", arr);
+
+// const result = arr.reduce((acc, num) => {
+//   if (num % 2 === 0) {
+//     acc.even++;
+//   } else {
+//     acc.odd++;
+//   }
+//   return acc;
+// }, { odd: 0, even: 0 });
+
+// console.log(result);
